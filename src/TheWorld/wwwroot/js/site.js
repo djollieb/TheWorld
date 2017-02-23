a618892f41ad685e81701d33e8cfa724d186339b
dj_ollie_b <hardcore>
2017-02-23 15:39:33 +0000

    Add project files.

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿// site.js
+
+var ele = document.getElementById("username");
+ele.innerHTML = "Ollie Badger";
+
+var main = document.getElementById("main");
+main.onmouseenter = function () {
+    main.style
+};
