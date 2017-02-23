d2920b136cbe164bb140e2ebcc175e41a4e7d448
Ollie <Ollie@OLLIE-PC>
2017-02-23 22:29:25 +0000

    23/02/2017 22:30

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,14 +1,30 @@
 ﻿// site.js
 (function () {
-    var ele = $("#username");
-    ele.innerHTML = "Ollie Badger";
+    //var ele = $("#username");
+    //ele.text = "Ollie Badger";
 
-    var main = $("#main");
-    main.on("mouseenter", function () {
-        main.style = "background-color: #888;";
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style = "background-color: #888;";
+    //});
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
     });
-
-    main.on("mouseleave", function () {
-        main.style = "backgroun-color: '';";
-    }
 })();
